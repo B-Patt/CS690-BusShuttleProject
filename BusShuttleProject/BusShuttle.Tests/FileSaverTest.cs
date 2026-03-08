20d173333313251daaf398a9f104f584b1874528
B-Patt <brett.patterson@bsu.edu>
2026-03-08 15:49:11 +0000

    initial tests

---
+++ BusShuttleProject/BusShuttle.Tests/FileSaverTest.cs
@@ -0,0 +1,23 @@
+﻿namespace BusShuttle.Tests;
+
+using BusShuttle;
+
+public class FileSaverTest
+{
+    FileSaver fileSaver;
+    string testFileName;
+    public FileSaverTest()
+        {
+            testFileName = "test-doc.txt";
+            fileSaver = new FileSaver(testFileName);
+        }   
+        
+        [Fact]
+        public void Test_FileSaver_Append()
+    {
+        fileSaver.AppendLine("Hello, World!");
+        var contentFromFile = File.ReadAllText(testFileName);
+        Assert.Equal("Hello, World!" + Environment.NewLine, contentFromFile);
+    }
+}
+
